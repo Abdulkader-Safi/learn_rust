fbe4008cca9410c1756d6e266b04330de3a6561e
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-06 21:43:41 +0300

    cli args

---
+++ src/cli.rs
@@ -0,0 +1,15 @@
+pub fn run() {
+    let args: Vec<String> = std::env::args().collect();
+    let command = args[1].clone();
+    let name = "Abdulkader";
+    let status = "100%";
+
+    println!("Args: {:?}", args);
+    println!("Command: {}", command);
+
+    if command == "hello" {
+        println!("Hi {}!, how are you?", name)
+    } else if command == "status" {
+        println!("Status {}", status)
+    }
+}
