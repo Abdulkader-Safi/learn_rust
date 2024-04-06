a528404a15719beeab4fb37327b4a4e294b83858
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-06 17:04:41 +0300

    Tuple Struct

--- src/structs.rs
+++ src/structs.rs
@@ -7,15 +7,25 @@ struct Color {
     blue: u8,
 }
 
+// Tuple Struct
+struct Color2(u8, u8, u8);
+
 pub fn run() {
+    // Traditional Struct
     let mut c = Color {
         red: 255,
         green: 0,
         blue: 0,
     };
-
     println!("Color: {} {} {}", c.red, c.blue, c.green);
 
     c.red = 200;
     println!("Color: {} {} {}", c.red, c.blue, c.green);
+
+    // Tuple Struct
+    let mut c2 = Color2(255, 0, 0);
+    println!("Color: {} {} {}", c2.0, c2.1, c2.2);
+
+    c2.1 = 200;
+    println!("Color: {} {} {}", c2.0, c2.1, c2.2);
 }
