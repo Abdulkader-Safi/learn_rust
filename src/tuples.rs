fdaed990de77961f2d8658d27cf55acd4c4368e4
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-06 10:10:18 +0300

    tuples

---
+++ src/tuples.rs
@@ -0,0 +1,8 @@
+// Tuples group together value of different types
+// Max 12 elements
+
+pub fn run() {
+    let person: (&str, &str, i8) = ("Abdulkader", "Lebanon", 24);
+
+    println!("{} is from {} and is {}", person.0, person.1, person.2);
+}
