9212378bb73fa0cd4daaf1bbb9b97d2c0c34b3fa
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-06 04:33:55 +0300

    types

---
+++ src/types.rs
@@ -0,0 +1,12 @@
+// Primitive types
+/*
+ * Integers: u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, (number of bits they take in the memory)
+ *              u unsigned can not have a negative number
+ * Floats: f32, f64
+ * Boolean (boll)
+ * Characters (chr)
+ * Tuples
+ * Arrays
+ */
+
+pub fn run() {}
