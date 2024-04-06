82b842ce91b2ab4a36afd197a29601f7877526a5
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-04-06 21:25:15 +0300

    function related to struct

--- src/structs.rs
+++ src/structs.rs
@@ -10,6 +10,32 @@ struct Color {
 // Tuple Struct
 struct Color2(u8, u8, u8);
 
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+// Function the work with Person struct
+impl Person {
+    // Construct Person
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+
+    // Get Full Name
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
+
+    // Set Last Name
+    fn set_last_name(&mut self, last: &str) {
+        self.last_name = last.to_string()
+    }
+}
+
 pub fn run() {
     // Traditional Struct
     let mut c = Color {
@@ -26,6 +52,14 @@ pub fn run() {
     let mut c2 = Color2(255, 0, 0);
     println!("Color: {} {} {}", c2.0, c2.1, c2.2);
 
-    c2.1 = 200;
+    c2.0 = 0;
+    c2.1 = 255;
+    c2.2 = 0;
     println!("Color: {} {} {}", c2.0, c2.1, c2.2);
+
+    let mut p = Person::new("Abdulkader", "");
+    println!("Person {} {}", p.first_name, p.last_name);
+
+    p.set_last_name("Safi");
+    println!("Person full Name: {}", p.full_name())
 }
